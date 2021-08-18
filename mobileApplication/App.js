2dcadfa727f11103a280aad4afb67406252f93c5
info-arnav <info@arnavgupta.net>
2021-08-18 10:35:02 +0000

    any sentence

--- mobileApplication/App.js
+++ mobileApplication/App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {useState} from "react"
+import { TextInput, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useEffect(()=>{}, [value])
+  const [value, setValue] = useState("0")
+  const [value1, setValue1] = useState("0")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{fontSize : "50px", fontWeight:"bold"}}>This is my first applications</Text>
+      <Text style={{fontSize : "50px"}}>This is my first applications</Text>
+       {
+      //() => {} this means a function
+      // use // for comments
+      }
+      <TextInput onChange={(e)=>{setValue(e.target.value)}}
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        
+      /><TextInput onChange={(e)=>{setValue1(e.target.value)}}
+      style={{height: 40}}
+      placeholder="Type here to translate!"
+      
+    />
+    <Text>{value}</Text>
+      <Text>{value1}</Text>
+      <Text>{parseInt(value) + parseInt(value1)}</Text> 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
