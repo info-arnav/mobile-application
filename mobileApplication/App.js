c5080fd3a9fba213965e60ee2c96217134dfae76
info-arnav <info@arnavgupta.net>
2021-08-25 11:53:51 +0000

    calculator

--- mobileApplication/App.js
+++ mobileApplication/App.js
@@ -1,32 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import {useState} from "react"
+import React, { useState } from 'react';
 import { TextInput, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  useEffect(()=>{}, [value])
-  const [value, setValue] = useState("0")
-  const [value1, setValue1] = useState("0")
+  const numberPress = (e) => {
+    setScreen(screen+e.target.innerText)
+  }
+  const operations = (e) => {
+    setNumber1(parseInt(screen))
+    setOperation(e.target.innerText)
+    setScreen("0")
+  }
+  const result = (e) => {
+    if (operation == "+") {
+      setNumber1(number1+parseInt(screen))
+      setScreen(number1.toString())
+    }else if (operation == "-") {
+      setNumber1((number1-parseInt(screen)))
+      setScreen(number1.toString())
+    }else if (operation == "*") {
+      setNumber1((number1*parseInt(screen)))
+      setScreen(number1.toString())
+    }else if (operation == "/") {
+      setNumber1((number1/parseInt(screen)))
+      setScreen(number1.toString())
+    }
+  }
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+  const [operation, setOperation] = useState("+")
+  const [screen, setScreen] = useState("0")
   return (
     <View>
-      <Text style={{fontSize : "50px", fontWeight:"bold"}}>This is my first applications</Text>
-      <Text style={{fontSize : "50px"}}>This is my first applications</Text>
-       {
-      //() => {} this means a function
-      // use // for comments
-      }
-      <TextInput onChange={(e)=>{setValue(e.target.value)}}
-        style={{height: 40}}
-        placeholder="Type here to translate!"
-        
-      /><TextInput onChange={(e)=>{setValue1(e.target.value)}}
-      style={{height: 40}}
-      placeholder="Type here to translate!"
-      
-    />
-    <Text>{value}</Text>
-      <Text>{value1}</Text>
-      <Text>{parseInt(value) + parseInt(value1)}</Text> 
+      <div style={{height:"100%", width:"100%"}}>
+      <Text style={{fontSize:20, color:"white", backgroundColor:"black",width:"100%"}}>{parseInt(screen)}</Text>
+      <div>
+        <Button color="black" onPress = {operations} title="+" style={{marginBottom:10}}>+</Button>
+        <Button color="black" onPress = {operations} title="-" style={{marginBottom:10}}>-</Button>
+        <Button color="black" onPress = {operations} title="/" style={{marginBottom:10}}>/</Button>
+        <Button color="black" onPress = {operations} title="*" style={{marginBottom:10}}>*</Button>
+      </div>
+      <div>
+        <Button color="gray" onPress={numberPress} title="9" style={{marginBottom:10}}>9</Button>
+        <Button color="gray" onPress={numberPress} title="8"style={{marginBottom:10}}>8</Button>
+        <Button color="gray" onPress={numberPress} title="7"style={{marginBottom:10}}>7</Button>
+        <Button color="gray" onPress={numberPress} title="6"style={{marginBottom:10}}>6</Button>
+      </div>
+      <div>
+        <Button color="gray" onPress={numberPress} title="5"style={{marginBottom:10}}>5</Button>
+        <Button color="gray" onPress={numberPress} title="4"style={{marginBottom:10}}>4</Button>
+        <Button color="gray" onPress={numberPress} title="3"style={{marginBottom:10}}>3</Button>
+        <Button color="gray" onPress={numberPress} title="2"style={{marginBottom:10}}>2</Button>
+      </div>
+      <div>
+        <Button color="gray" onPress={numberPress} title="1"style={{marginBottom:10}}>1</Button>
+        <Button color="gray" onPress={numberPress} title="0"style={{marginBottom:10}}>0</Button>
+        <Button color="gray" onPress={result} title="="style={{marginBottom:10}}>=</Button>
+        <Button color="black"onPress = {()=>{setScreen("0")}}title="C"style={{marginBottom:10}}></Button>
+      </div>
+      </div>
     </View>
   );
 }
